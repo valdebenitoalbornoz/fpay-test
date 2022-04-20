68834c669331c0edda763fdaa842cac448e3a1b0
Daniel Valdebenito Albornoz <daniel.valdebenito@idiem.cl>
2022-04-20 01:03:53 -0400

    Remove app rule from infrastructure

--- src/infrastructure/api/routes/PrimesRouter.ts
+++ src/infrastructure/api/routes/PrimesRouter.ts
@@ -2,6 +2,8 @@ import { Application } from "express";
 import { Router } from "./Router";
 import { PrimesController } from "../controllers/PrimesController";
 import { param } from "express-validator";
+
+
 export class PrimesRouter implements Router {
     controller = new PrimesController();
     constructor(private app: Application) {
@@ -12,7 +14,6 @@ export class PrimesRouter implements Router {
             param('limit')
                 .isNumeric()
                 .withMessage('El parámetro debe ser un valor numérico')
-                .isInt()
                 .withMessage('El parámetro debe ser un valor entero'),
             this.controller.run
         )    
